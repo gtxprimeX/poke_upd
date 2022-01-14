86967669d8b3d5ede6d32638dbc41a235a1a6941
gtxprimeX <97587202+gtxprimeX@users.noreply.github.com>
2022-01-14 18:51:04 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -172,7 +172,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                 media.file_type = message.media
                 media.caption = message.caption
                 media.file_name = message.caption
-                media.mime_type = image/gif
+                media.mime_type = "image/gif"
                 aynav, vnay = await save_file(media)
                 if aynav:
                     total_files += 1
