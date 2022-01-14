355187361ef2fe655932f1ae98a17f0a33f275e9
gtxprimeX <97587202+gtxprimeX@users.noreply.github.com>
2022-01-14 18:59:33 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -169,7 +169,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                 if not media:
                     unsupported += 1
                     continue    
-                if message.media in ["photo"]:
+                    if message.media in ["photo"]:
                 media.file_type = message.media
                 media.caption = message.caption
                 media.file_name = message.caption
