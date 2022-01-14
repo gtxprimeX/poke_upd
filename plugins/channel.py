a34f375960473b7e376fb25d163197c37278d43d
gtxprimeX <97587202+gtxprimeX@users.noreply.github.com>
2022-01-14 18:14:24 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -2,13 +2,13 @@ from pyrogram import Client, filters
 from info import CHANNELS
 from database.ia_filterdb import save_file
 
-media_filter = filters.document | filters.video | filters.audio | filters.photo
+media_filter = filters.document | filters.video | filters.audio | filters.images
 
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
     """Media Handler"""
-    for file_type in ("document", "video", "audio", "photo"):
+    for file_type in ("document", "video", "audio", "images"):
         media = getattr(message, file_type, None)
         if media is not None:
             break
