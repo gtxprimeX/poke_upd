088a9a61ff79b912e57ebdee4917af2846249127
gtxprimeX <97587202+gtxprimeX@users.noreply.github.com>
2022-01-15 13:18:58 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -162,15 +162,23 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                 elif not message.media:
                     no_media += 1
                     continue
-                elif message.media not in ['audio', 'video', 'document']:
+                elif message.media not in ['audio', 'video', 'document', 'photo']:
                     unsupported += 1
                     continue
                 media = getattr(message, message.media, None)
                 if not media:
                     unsupported += 1
                     continue
+                if message.media in ['photo']
                 media.file_type = message.media
                 media.caption = message.caption
+                media.file_name = message.caption
+                media.mime_type = 'photo/gif'
+                continue
+                elif 
+                media.file_type = message.media
+                media.caption = message.caption
+                continue
                 aynav, vnay = await save_file(media)
                 if aynav:
                     total_files += 1
