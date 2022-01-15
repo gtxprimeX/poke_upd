4503127bb596fc67e50de7d0fcb9b92bc914621c
gtxprimeX <97587202+gtxprimeX@users.noreply.github.com>
2022-01-15 14:52:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -289,7 +289,7 @@ async def delete(bot, message):
     if result.deleted_count:
         await msg.edit('File is successfully deleted from database')
     else:
-        file_name = re.sub(r"(_|\-|\.|\+)", " ", str((media.file_name))
+        file_name = re.sub(r"(_|\-|\.|\+)", " ", str(media.file_name))
         result = await Media.collection.delete_one({
             'file_name': file_name,
             'file_size': media.file_size,
@@ -301,7 +301,7 @@ async def delete(bot, message):
             # files indexed before https://github.com/EvamariaTG/EvaMaria/commit/f3d2a1bcb155faf44178e5d7a685a1b533e714bf#diff-86b613edf1748372103e94cacff3b578b36b698ef9c16817bb98fe9ef22fb669R39 
             # have original file name.
             result = await Media.collection.delete_one({
-                'file_name': media.file_name
+                'file_name': media.file_name,
                 'file_size': media.file_size,
                 'mime_type': media.mime_type
             })
